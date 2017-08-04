2b520fcb1137d761c2474f67eeb69630b64108ab
cailmdaley <cailmdaley@gmail.com>
2017-08-03 22:01:11 -0400

    restructured things

--- fitting.py
+++ fitting.py
@@ -1,7 +1,11 @@
 from astropy.io import fits
 import numpy as np
+import pandas as pd
 import subprocess as sp
 import os
+from emcee.utils import MPIPool
+import matplotlib.pyplot as plt
+import emcee
 
 class Observation:
     def __init__(self, root, name,  rms):
@@ -79,36 +83,54 @@ class Model:
         self.path = root + name
         self.chis = []
         
+        self.delete() # delete any preexisting files that will conflict
+        
     def delete(self):
         sp.call('rm -rf {}*'.format(self.path), shell=True)
 
         
-    def obs_sample(self, obs):
+    def obs_sample(self, obs, suffix=''):
         """
         Create model fits file with correct header information and sample using
         ALMA observation uv coverage to to create model .vis and .uvf files.
         """
 
         # define observation-specific model name and delete any preexisting conditions
-        sp.call('rm -rf {}{{.im,.vis,.uvf}}'.format(self.path), shell=True)
+        sp.call('rm -rf {}{{.im,.vis,.uvf}}'.format(self.path + suffix), shell=True)
 
         # Convert model into MIRIAD .im image file
         sp.call(['fits', 'op=xyin',
             'in={}.fits'.format(self.path),
-            'out={}.im'.format(self.path)], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
+            'out={}.im'.format(self.path + suffix)], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
 
         # Sample the model image using the observation uv coverage
         sp.call(['uvmodel', 'options=replace',
             'vis={}.vis'.format(obs.path),
-            'model={}.im'.format(self.path),
-            'out={}.vis'.format(self.path)], stdout=open(os.devnull, 'wb'))
+            'model={}.im'.format(self.path + suffix),
+            'out={}.vis'.format(self.path + suffix)], stdout=open(os.devnull, 'wb'))
 
         #Convert to UVfits
         sp.call(['fits', 'op=uvout',
-            'in={}.vis'.format(self.path),
-            'out={}.uvf'.format(self.path)], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
+            'in={}.vis'.format(self.path + suffix),
+            'out={}.uvf'.format(self.path + suffix)], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
+            
+    def make_residuals(self, obs, suffix='', show=False):
 
-    def get_chi(self, obs):
+        """
+        Create model residuals (data - model), and clean//display if desired
+        """
+        sp.call('rm -rf *.residual.vis'.format(self.path + suffix), shell=True)
+
+        # Subtract model visibilities from data; outfile is residual visibilities
+        sp.call(['uvmodel', 'options=subtract',
+            'vis={}.vis'.format(obs.path),
+            'model={}.im'.format(self.path + suffix),
+            'out={}.residuals.vis'.format(self.path + suffix)], stdout=open(os.devnull, 'wb'))
+
+        if show == True:
+            self.clean(obs, residual=True)
+
+    def get_chi(self, obs, suffix=''):
         """
         Return chi^2 statistics of model.
         """
@@ -122,7 +144,7 @@ class Model:
         datrl_stokes = np.array((datrl_xx + datrl_yy) / 2.)
         datim_stokes = np.array((datim_xx + datim_yy) / 2.)
 
-        uvf  = fits.open(self.path + '.uvf')
+        uvf  = fits.open(self.path + suffix + '.uvf')
         modrlimwt = uvf[0].data['data']
         modrl_stokes = modrlimwt[::2, 0, 0, 0, 0, 0]
         modim_stokes = modrlimwt[::2, 0, 0, 0, 0, 1]
@@ -184,20 +206,85 @@ class Model:
                 'region=arcsec,box(-5,-5,5,5)',
                 'labtyp=arcsec', 'beamtyp=b,l,3',])
 
-    def residuals(self, obs, show=True):
 
-        """
-        Create model residuals (data - model), and clean//display if desired
-        """
+class MCMCrun:
+    def __init__(self, name, nwalkers, burn_in=0):
+        self.name = name 
+        self.nwalkers = nwalkers
+        self.burn_in = burn_in
+        
+        # read in chain from .csv
+        self.chain = pd.read_csv(name + '/chain.csv')
+    
+        # indentify bad walkers and make clean chain
+        last_step = self.chain.iloc[-nwalkers:]
+        self.last_step = self.chain.iloc[-nwalkers:]
+        self.bad_walkers = last_step[last_step['lnprob'] == -np.inf].index % self.nwalkers
+        self.clean_chain = self.chain.drop([row for row in self.chain.index 
+            if row%self.nwalkers in self.bad_walkers])
+        if len(self.bad_walkers !=0):
+            print('walkers {} have not converged'.format(tuple(self.bad_walkers)))
+        
+        
+    def evolution(self, show=False):
+        plt.close()
+        steps=self.chain.index // self.nwalkers
 
-        #Set observation-specific filepath
-        filepath = self.root + self.name + obs.shortname
+        color = 'red' if 0 in self.bad_walkers else 'black'
+        axes = self.chain.iloc[0::self.nwalkers].plot(x=np.arange(steps[-1]+1),
+            figsize=(7, 2.2*(len(self.chain.columns))), subplots=True, color=color, alpha=0.5)
+            
+        for i in range(self.nwalkers-1):
+            color = 'red' if i+1 in self.bad_walkers else 'black'
+            self.chain.iloc[i+1::self.nwalkers].plot(x=np.arange(steps[-1]+1),
+                subplots=True, ax=axes, legend=False, color=color, alpha=0.5)
 
-        # Subtract model visibilities from data; outfile is residual visibilities
-        sp.call(['uvmodel', 'options=subtract',
-            'model={}.im'.format(filepath),
-            'vis={}.vis'.format(obs.path),
-            'out={}.residual.vis'.format(filepath)])
+        # remove bad walkers before taking mean
+        stepped_chain = self.clean_chain.set_index(steps)
 
-        if show == True:
-            self.clean(obs, residual=True)
+        walker_means = pd.DataFrame([stepped_chain.loc[i].mean() for i in range(stepped_chain.index[-1])])
+        walker_means.plot(subplots=True, ax=axes, legend=False, color='forestgreen', ls='--')
+
+        plt.suptitle(self.name + ' walker evolution')
+        plt.savefig('{}/evolution.png'.format(self.name), dpi=700)
+        if show:
+            plt.show()
+
+def run_emcee(run_name, nsteps, nwalkers, lnprob, to_vary):
+    pool = MPIPool()
+    if not pool.is_master():
+        pool.wait()
+        sys.exit(0)
+        
+    # initiate sampler chain
+    ndim = len(to_vary)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(run_name, to_vary), pool=pool)
+
+    try:
+        chain = pd.read_csv(run_name + '/chain.csv')
+        start_step = chain.index[-1] // nwalkers
+        print('Resuming {} at step {}'.format(run_name, start_step))
+        pos = np.array(chain.iloc[-nwalkers:, :-1])
+        with open(run_name + '/chain.csv', 'a') as f:
+            f.write('\n')
+    except IOError:
+        
+        sp.call(['mkdir', run_name])
+        sp.call(['mkdir', run_name + '/model_files'])
+        
+        print('Starting {}'.format(run_name))
+        start_step = 0
+        
+        with open(run_name + '/chain.csv', 'w') as f:
+            np.savetxt(f, (np.append([param[0] for param in to_vary], 'lnprob\n'),), 
+                delimiter=',', fmt='%s')
+        pos = [[param[1] + param[2]*np.random.randn() for param in to_vary] 
+            for i in range(nwalkers)] 
+            
+    for i, result in enumerate(sampler.sample(pos, iterations=nsteps, storechain=False)):
+        print("Step {}".format(start_step + i))
+        pos, chisum, blob = result
+        with open(run_name + '/chain.csv', 'a') as f: 
+            np.savetxt(f, [np.append(pos[i], chisum[i]) for i in range(nwalkers)], delimiter=',')
+
+    pool.close()
