af30c989d1d8c4060962847af9e54eda0054acbb
cailmdaley <cailmdaley@gmail.com>
2017-08-15 13:58:45 -0400

    sirius: did some work on the MCMCrun class

--- mcmc.py
+++ mcmc.py
@@ -8,51 +8,59 @@ import emcee
 
 class MCMCrun:
     def __init__(self, name, nwalkers, burn_in=0):
+        
+        # read in chain from .csv
+        self.main = pd.read_csv(name + '/' + name + '_chain.csv')
         self.name = name 
         self.nwalkers = nwalkers
         self.burn_in = burn_in
+        self.nsteps = self.main.shape[0] / nwalkers
         
-        # read in chain from .csv
-        self.chain = pd.read_csv(name + '/' + name + '_chain.csv')
-    
-        # indentify bad walkers and make clean chain
+        
+        # indentify bad walkers 
         print('Identifying bad walkers...')
-        last_steps = self.chain.iloc[-nwalkers*1000:]
+        last_steps = self.main.iloc[self.nsteps/2:]
         last_steps.index %= nwalkers
         self.bad_walkers = []
         for i in range(nwalkers):
             if last_steps.loc[i].duplicated(keep=False).sum() == len(last_steps) / nwalkers:
                 self.bad_walkers.append(i)
                 
-        self.clean_chain = self.chain.drop([row for row in self.chain.index 
+        print('Walkers {} have not converged.'.format(tuple(self.bad_walkers)))
+                
+        self.converged = self.main.drop([row for row in self.main.index 
             if row%nwalkers in self.bad_walkers])
-            
-        if len(self.bad_walkers) !=0:
-            print('Walkers {} have not converged.'.format(tuple(self.bad_walkers)))
+        self.burnt_in = self.main.iloc[burn_in*nwalkers:]
+        self.no_infs = self.main[self.main['lnprob'] != -np.inf]
         
+        self.groomed = self.converged.merge(self.burnt_in.merge(self.no_infs, how='inner'), how='inner')
         
     def evolution(self, show=False):
         print('Making walker evolution plot...')
         plt.close()
+        
+        main = self.main.copy()
+        main['lnprob'][np.isinf(main['lnprob'])] = np.nan
+        
+        
 
-        print(self.chain.shape)
-        print(self.chain.shape[0]/float(self.nwalkers))
         color = 'red' if 0 in self.bad_walkers else 'black'
-        axes = self.chain.iloc[0::self.nwalkers].plot(x=np.arange(self.chain.shape[0]+1),
-            figsize=(7, 2.2*(len(self.chain.columns))), subplots=True, color=color, alpha=0.5)
-        print(len(self.chain.iloc[0::self.nwalkers]))
+        axes = main.iloc[0::self.nwalkers].plot(
+            x=np.arange(self.nsteps), figsize=(7, 2.2*(len(main.columns))), 
+            subplots=True, color=color, alpha=0.5)
             
         for i in range(self.nwalkers-1):
-            print(len(self.chain.iloc[i+1::self.nwalkers]))
             color = 'red' if i+1 in self.bad_walkers else 'black'
-            self.chain.iloc[i+1::self.nwalkers].plot(x=np.arange(x=np.arange(self.chain.shape[0]+1)),
-                subplots=True, ax=axes, legend=False, color=color, alpha=0.5)
+            main.iloc[i+1::self.nwalkers].plot(
+                x=np.arange(self.nsteps), subplots=True, ax=axes, 
+                legend=False, color=color, alpha=0.5)
 
-        # remove bad walkers before taking mean
-        mean_chain = self.chain.copy()
-        mean_chain.index //= self.nwalkers-len(self.bad_walkers)
-
-        walker_means = pd.DataFrame([mean_chain.loc[i].mean() for i in range(mean_chain.index[-1])])
+        
+        main[np.isnan(main['lnprob'])] = np.nan
+        print(main[main['lnprob'] == np.nan])
+        main.index //= self.nwalkers
+        
+        walker_means = pd.DataFrame([main.loc[i].mean() for i in range(self.nsteps)])
         walker_means.plot(subplots=True, ax=axes, legend=False, color='forestgreen', ls='--')
 
         plt.suptitle(self.name + ' walker evolution')
@@ -60,58 +68,49 @@ class MCMCrun:
         if show:
             plt.show()
 
-# def corner_plot(run_name, nwalkers, stat_specs, burn_in=0, bad_walkers=[]):
-#     """ Plot 'corner plot' of fit"""
-#     plt.close()
-# 
-#     # read in samples
-#     samples = pd.read_csv(run_name + '.csv')
-#     rename(samples)
-# 
-#     # cut out burn in and bad walkers
-#     # posterior = samples.iloc[-100:, :-1]
-#     posterior = samples.iloc[burn_in*nwalkers:, :-1]
-# 
-#     last_step = samples.iloc[-nwalkers:]
-#     bad_walkers = last_step[last_step['lnprob'] == -np.inf].index % nwalkers
-#     posterior.drop([row for row in posterior.index if row%nwalkers in bad_walkers], inplace=True)
-#     print('walkers {} removed from posterior.'.format(tuple(bad_walkers)))
-# 
-#     # make corner plot
-#     corner = sns.PairGrid(posterior, diag_sharey=False, despine=False)
-#     corner.map_diag(sns.kdeplot, cut=0)
-#     corner.map_lower(sns.kdeplot, cut=0, cmap='Blues', n_levels=3, shade=True)
-#     corner.map_lower(plt.scatter, s=1, color='#708090', alpha=0.2)
-#     corner.map_lower(sns.kdeplot, cut=0, cmap='Blues', n_levels=3, shade=False)
-# 
-#     # get best_fit and posterior statistics
-#     stats = posterior.describe().drop(['count', 'min', 'max'])
-#     stats.loc['best fit'] = samples.drop('lnprob', 1).loc[samples['lnprob'].idxmax()]
-#     stats = stats.iloc[[-1]].append(stats.iloc[:-1])
-#     print(stats.round(2).to_latex())
-# 
-#     table_ax = corner.fig.add_axes([0,0,1,1], frameon=False)
-#     table_ax.axis('off')
-#     left, bottom = stat_specs
-#     pd.plotting.table(table_ax, stats.round(2), bbox=[left, bottom, 1-left, .12], edges='open', colLoc='right')
-# 
-#     # hide upper triangle, so that it's a conventional corner plot
-#     for i, j in zip(*np.triu_indices_from(corner.axes, 1)):
-#         corner.axes[i, j].set_visible(False)
-# 
-#     for ax in corner.axes.flat:
-#         ax.xaxis.set_major_formatter(FormatStrFormatter('%.3g'))
-#         ax.yaxis.set_major_formatter(FormatStrFormatter('%.3g'))
-# 
-#     if burn_in == 0:
-#         title = run_name + '.corner_ungroomed'
-#     else:
-#         title = run_name + '.corner_groomed'
-# 
-#     plt.subplots_adjust(top=0.9)
-#     corner.fig.suptitle(r'{} Parameters, {} Walkers, {} Steps $\to$ {} Samples'
-#         .format(posterior.shape[1], nwalkers, posterior.shape[0]//nwalkers, posterior.shape[0], fontsize=25))
-#     corner.savefig(title + '.png', dpi=700)
+    def kde(self):
+        print('Generating posterior kde plots...')
+        self.groomed.plot(kind='kde', subplots=True)
+        
+        plt.savefig(self.name + '/' + self.name + '_kde.pdf'.format(self.name), dpi=700)
+        plt.show()
+        
+def corner(run_name, nwalkers, stat_specs, burn_in=0, bad_walkers=[]):
+    """ Plot 'corner plot' of fit"""
+    plt.close()
+
+    # make corner plot
+    corner = sns.PairGrid(self.groomed, diag_sharey=False, despine=False)
+    corner.map_diag(sns.kdeplot, cut=0)
+    corner.map_lower(sns.kdeplot, cut=0, cmap='Blues', n_levels=3, shade=True)
+    corner.map_lower(plt.scatter, s=1, color='#708090', alpha=0.2)
+    corner.map_lower(sns.kdeplot, cut=0, cmap='Blues', n_levels=3, shade=False)
+
+    # get best_fit and posterior statistics
+    stats = self.groomed.describe().drop(['count', 'min', 'max'])
+    stats.loc['best fit'] = samples.drop('lnprob', 1).loc[samples['lnprob'].idxmax()]
+    stats = stats.iloc[[-1]].append(stats.iloc[:-1])
+    # print(stats.round(2).to_latex())
+
+    table_ax = corner.fig.add_axes([0,0,1,1], frameon=False)
+    table_ax.axis('off')
+    left, bottom = stat_specs
+    pd.plotting.table(table_ax, stats.round(2), bbox=[left, bottom, 1-left, .12], edges='open', colLoc='right')
+
+    # hide upper triangle, so that it's a conventional corner plot
+    for i, j in zip(*np.triu_indices_from(corner.axes, 1)):
+        corner.axes[i, j].set_visible(False)
+
+    for ax in corner.axes.flat:
+        ax.xaxis.set_major_formatter(FormatStrFormatter('%.3g'))
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.3g'))
+
+        title = self.name + 'Corner Plot'
+
+    plt.subplots_adjust(top=0.9)
+    corner.fig.suptitle(r'{} Parameters, {} Walkers, {} Steps $\to$ {} Samples'
+        .format(posterior.shape[1], nwalkers, posterior.shape[0]//nwalkers, posterior.shape[0], fontsize=25))
+    plt.savefig(self.name + '/' + self.name + '_corner.pdf'.format(self.name), dpi=700)
 
 def run_emcee(run_name, nsteps, nwalkers, lnprob, to_vary):
     pool = MPIPool()
